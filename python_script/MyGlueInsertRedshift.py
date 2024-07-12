a19a8ef53b9b7def7ea17b7a5b114e5cdac44df3
Naveen Kumar <navin5556@gmail.com>
2024-07-12 11:47:30 +0530

    correcting last commit-file name is MyGlueInsertRedshift.py

--- python_script/MyGlueInsertRedshift.py
+++ python_script/MyGlueInsertRedshift.py
@@ -18,7 +18,7 @@ S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
     format_options={}, 
     connection_type="s3", 
     format="parquet", 
-    connection_options={"paths": ["s3://myglue-etl-project-s3-bucket/Output/"], "recurse": True}, 
+    connection_options={"paths": ["s3://myglue-etl-project-s3-bucket/Output/"], "recurse": True},
     transformation_ctx="S3bucket_node1"
     )
 
@@ -38,12 +38,12 @@ AmazonRedshift_node3 = glueContext.write_dynamic_frame.from_options(
     frame=ApplyMapping_node2, 
     connection_type="redshift", 
     connection_options={
-        "redshiftTmpDir": "s3://aws-glue-assets-535560865181-ap-south-1/temporary/", 
+        "redshiftTmpDir": "s3://aws-glue-assets-535560865181-ap-south-1/temporary/",
         "useConnectionProperties": "true", 
         "dbtable": "public.product_tab_def", 
         "connectionName": "myredshiftcluster-connection", 
         "preactions": "CREATE TABLE IF NOT EXISTS public.product_tab_def (new_year VARCHAR, cnt BIGINT, qty BIGINT);"
-        }, 
+        },
         transformation_ctx="AmazonRedshift_node3"
         )
 
